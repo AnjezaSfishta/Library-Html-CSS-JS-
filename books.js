de17bc6dfcf88db7fe4a9b7e8ea30747114de6ec
Dion-Kastrati <122050394+Dion-Kastrati@users.noreply.github.com>
2023-01-18 22:07:32 +0100

    Update

---
+++ books.js
@@ -0,0 +1,11 @@
+document.getElementById("count1").innerHTML = "10";
+document.getElementById("count2").innerHTML = "6";
+document.getElementById("count3").innerHTML = 17;
+document.getElementById("count4").innerHTML = 2;
+document.getElementById("count5").innerHTML = 13;
+document.getElementById("count6").innerHTML = 12;
+
+
+function addToCardOnClick(){
+    alert("Added to card successfully");
+}
