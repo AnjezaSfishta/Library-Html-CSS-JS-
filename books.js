e8354bff3d0a294f137b4929da1aa607e0710434
Dion-Kastrati <122050394+Dion-Kastrati@users.noreply.github.com>
2023-01-19 03:01:00 +0100

    Update

--- books.js
+++ books.js
@@ -1,11 +1,90 @@
-document.getElementById("count1").innerHTML = "10";
-document.getElementById("count2").innerHTML = "6";
-document.getElementById("count3").innerHTML = 17;
-document.getElementById("count4").innerHTML = 2;
-document.getElementById("count5").innerHTML = 13;
-document.getElementById("count6").innerHTML = 12;
-
-
-function addToCardOnClick(){
-    alert("Added to card successfully");
+function addToCard1() {
+    const quantity = document.getElementById("sasia1")
+    const soldOut = document.getElementById("soldText1")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
+}
+function addToCard2() {
+    const quantity = document.getElementById("sasia2")
+    const soldOut = document.getElementById("soldText2")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
+}
+function addToCard3() {
+    const quantity = document.getElementById("sasia3")
+    const soldOut = document.getElementById("soldText3")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
+}
+function addToCard4() {
+    const quantity = document.getElementById("sasia4")
+    const soldOut = document.getElementById("soldText4")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
+}
+function addToCard5() {
+    const quantity = document.getElementById("sasia5")
+    const soldOut = document.getElementById("soldText5")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
+}
+function addToCard6() {
+    const quantity = document.getElementById("sasia6")
+    const soldOut = document.getElementById("soldText6")
+    var finalValue = parseInt(quantity.innerHTML) - 1
+    quantity.innerHTML = finalValue
+    if (finalValue < 0) {
+        alert("Nuk ka me libra ne stock, prisni deri sa te rimbushet stock-u")
+        finalValue++
+        quantity.innerHTML = finalValue
+        soldOut.innerHTML = "SHITUR"
+    }
+    else if (finalValue >= 0) {
+        alert("Libri eshte shtuar ne shporte")
+    }
 }
